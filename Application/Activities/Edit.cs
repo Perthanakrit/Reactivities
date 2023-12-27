e7199bbed53faeb31e7379755efee3a7e84bc0b4
Per(k) <aupper094@gmail.com>
2023-12-27 23:53:54 +0700

    CRUD - backend (section4)

---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; } // This is the data we want to send to the API
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _mapper = mapper;
+                _context = context;
+            }
+
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activitiy = await _context.Activities.FindAsync(request.Activity.Id);
+
+                _mapper.Map(request.Activity, activitiy);
+
+                await _context.SaveChangesAsync();
+            }
+
+        }
+    }
+}
