3f297f795e41dc1473c0632049ecd1aea471f48c
Per(k) <aupper094@gmail.com>
2023-12-29 11:38:57 +0700

    Activity Features

--- client-app/src/App/layout/App.tsx
+++ client-app/src/App/layout/App.tsx
@@ -1,29 +1,77 @@
-import React, { useEffect, useState } from "react";
-import { Button, Header, List } from "semantic-ui-react";
+import React, { Fragment, useEffect, useState } from "react";
+import { Button, Container, Header, List, Menu } from "semantic-ui-react";
 import "./App.css";
 import axios from "axios";
+import { Activity } from "../models/activity";
+import Navbar from "./Navbar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import { v4 as uuid } from "uuid";
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<
+    Activity | undefined
+  >(undefined);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/activities").then((response) => {
-      //console.log(response);
-      setActivities(response.data);
-    });
+    axios
+      .get<Activity[]>("http://localhost:5000/api/activities")
+      .then((response) => {
+        //console.log(response);
+        setActivities(response.data);
+      });
   }, []);
 
+  function handleSelectedActivity(id: string) {
+    setSelectedActivity(activities.find((x) => x.id === id));
+  }
+
+  function handleCancelledActivity() {
+    setSelectedActivity(undefined);
+  }
+
+  function handleFormOpen(id?: string) {
+    id ? handleSelectedActivity(id) : handleCancelledActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
+  function handleCreateOrEditActivity(activity: Activity) {
+    activity.id
+      ? setActivities([
+          ...activities.filter((x) => x.id !== activity.id),
+          activity,
+        ])
+      : setActivities([...activities, { ...activity, id: uuid() }]);
+    setEditMode(false);
+    setSelectedActivity(activity);
+  }
+
+  function handleDeteleActivity(id: string) {
+    setActivities([...activities.filter((x) => x.id !== id)]);
+  }
+
   return (
-    <div className="App">
-      <Header as="h2" icon="users" content="Reactivities" />
-      <img src={""} className="App-logo" alt="logo" />
-      <List>
-        {activities.map((activity: any) => (
-          <List.Item key={activity.id}>{activity.title}</List.Item>
-        ))}
-      </List>
-      <Button content="test" />
-    </div>
+    <Fragment>
+      <Navbar openForm={handleFormOpen} />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard
+          activities={activities} /*activities=this.activities */
+          selectedActivity={selectedActivity}
+          selectActivity={handleSelectedActivity}
+          cancelSelectActivity={handleCancelledActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+          createOrEdit={handleCreateOrEditActivity}
+          deleteActivity={handleDeteleActivity}
+        />
+      </Container>
+    </Fragment>
   );
 }
 
